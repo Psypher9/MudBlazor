5a51cdff89e34f3ad7d9ae2768fc55c6a23bf785
Turner Bass <turner@astropanda.studio>
2021-10-08 22:44:44 -0500

    add context menu docs folder

---
+++ src/MudBlazor/Components/ContextMenu/MudContextualArea.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MudBlazor
+{
+    public partial class MudContextualArea : MudComponentBase
+    {
+        
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public Action<MouseEventArgs> OnContextMenu { get; set; }
+
+
+    }
+}
